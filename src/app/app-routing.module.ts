f62658fc0d3f93dd87bcdc7f1e9882c91fd1f78f
Ervin Triano <ervintdeveloper@gmail.com>
2023-03-23 19:01:22 -0600

    Implmentacion de Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { FacturasComponent } from './pages/facturas/facturas.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UsuariosComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'facturas', component: FacturasComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: '', component: UsuariosComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/' },
+];
+``;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [AppComponent],
